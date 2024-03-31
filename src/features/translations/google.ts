8e56d8cfa1fc2eaa0ccee979543905323ca0620d
wibus-wee <1596355173@qq.com>
2024-04-01 00:17:42 +0800

    fix(translate/google): convert zh iso code to zh-cn

--- src/features/translations/google.ts
+++ src/features/translations/google.ts
@@ -4,6 +4,16 @@ import type { TranslateFrom, TranslateTo } from '../../types/raycast/translate'
 
 export async function TranslateWithGoogle(request: FastifyRequest): Promise<TranslateTo> {
   const body = request.body as TranslateFrom
+
+  // Why need this?
+  // in @iamtraction/google-translate, Chinese is sperated into two types: zh-cn and zh-tw
+  // but in Raycast, the Simplified Chinese is zh and Traditional Chinese is zh-tw
+  // So we need to convert zh to zh-cn
+  if (body.source === 'zh')
+    body.source = 'zh-cn'
+  if (body.target === 'zh')
+    body.target = 'zh-cn'
+
   const res = await translate(body.q, {
     from: body.source,
     to: body.target,
