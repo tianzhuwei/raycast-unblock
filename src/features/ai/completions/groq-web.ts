5d304c59660837adb03cd6f7e0d8fdaa5621e515
Wibus <62133302+wibus-wee@users.noreply.github.com>
2024-04-21 08:10:24 +0800

    refactor(ai): universal ai completions handler (#61)

--- src/features/ai/completions/groq-web.ts
+++ src/features/ai/completions/groq-web.ts
@@ -1,77 +1,24 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
 import consola from 'consola'
-import destr from 'destr'
 import { getConfig } from '../../../utils/env.util'
-import type { RaycastCompletions } from '../../../types/raycast/completions'
 import { generateGroqWebRequestHeader } from '../../../services/groq-web'
 import { groqClient } from '../../../utils'
 import { GROQ_API_COMPLETIONS, GROQ_API_ENDPOINT } from '../../../services/groq-web/constants'
-import { processStream } from '../../../utils/stream-reader.util'
+import { PreUniversalAICompletions, UniversalAICompletions } from './universal'
 
 export async function GroqWebCompletions(request: FastifyRequest, reply: FastifyReply) {
-  const body = request.body as RaycastCompletions
-  const aiConfig = getConfig('ai')
   const config = getConfig('ai')?.groq
+  const body = (await PreUniversalAICompletions(request, config)).body
 
-  let temperature = config?.temperature || aiConfig?.temperature || 0.5
-  const requestBody = {
-    messages: [] as any[],
-    model: body.model,
-    temperature,
-    top_p: 1,
-    stream: true,
-    max_tokens: config?.maxTokens || aiConfig?.maxTokens,
-  }
   const headers = await generateGroqWebRequestHeader()
-  const messages = body.messages
-  for (const message of messages) {
-    if ('system_instructions' in message.content) {
-      requestBody.messages.push(
-        {
-          role: 'system',
-          content: message.content.system_instructions,
-        },
-      )
-    }
-
-    if ('command_instructions' in message.content) {
-      requestBody.messages.push(
-        {
-          role: 'system',
-          content: message.content.command_instructions,
-        },
-      )
-    }
-
-    if ('additional_system_instructions' in body) {
-      requestBody.messages.push(
-        {
-          role: 'system',
-          content: body.additional_system_instructions,
-        },
-      )
-    }
-
-    if ('text' in message.content) {
-      requestBody.messages.push(
-        {
-          role: message.author,
-          content: message.content.text,
-        },
-      )
-    }
-
-    if ('temperature' in message.content)
-      temperature = message.content.temperature
-  }
 
   const res = await groqClient.native(`${GROQ_API_ENDPOINT}${GROQ_API_COMPLETIONS}`, {
     method: 'POST',
     headers,
-    body: JSON.stringify(requestBody),
+    body: JSON.stringify(body),
   })
     .catch((e: any) => {
-      consola.error(`[Groq-Web] Request error: ${e.message}.`)
+      consola.error(`[Groq-Web] Request error: ${e}.`)
       throw new Error(`Request error: ${e.message}`)
     })
 
@@ -80,32 +27,5 @@ export async function GroqWebCompletions(request: FastifyRequest, reply: Fastify
     throw new Error(`Request error: ${res.statusText}`)
   }
 
-  const stream = processStream(res).stream as any
-
-  return reply.sse((async function * source() {
-    try {
-      for await (const data of stream) {
-        const json = destr(data) as any
-        const res = {
-          text: json.choices[0]?.delta.content || '',
-        }
-        yield { data: JSON.stringify(res) }
-      }
-    }
-    catch (e: any) {
-      console.error('Error: ', e.message)
-      const res = {
-        text: '',
-        finish_reason: e.message,
-      }
-      yield { data: JSON.stringify(res) }
-    }
-    finally {
-      const res = {
-        text: '',
-        finish_reason: 'stop',
-      }
-      yield { data: JSON.stringify(res) }
-    }
-  })())
+  return UniversalAICompletions(reply, res)
 }
---
+++ src/features/ai/completions/universal.ts
@@ -0,0 +1,109 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import destr from 'destr'
+import type { RaycastCompletions } from '../../../types/raycast/completions'
+import { getConfig } from '../../../utils/env.util'
+import { processStream } from '../../../utils/stream-reader.util'
+
+/**
+ * Prepare the request body for the AI
+ * @param request Fastify request
+ * @param config the settings for the AI
+ * @param reqBody any additional request body
+ * @returns the request body and temperature
+ */
+export async function PreUniversalAICompletions(request: FastifyRequest, config?: any, reqBody?: Record<string, any>) {
+  const body = request.body as RaycastCompletions
+  const aiConfig = getConfig('ai')
+  let temperature = config?.temperature || aiConfig?.temperature || 0.5
+  const requestBody = {
+    messages: [] as any[],
+    model: body.model,
+    temperature,
+    top_p: 1,
+    stream: true,
+    max_tokens: config?.maxTokens || aiConfig?.maxTokens,
+    ...reqBody,
+  }
+
+  const messages = body.messages
+  for (const message of messages) {
+    if ('system_instructions' in message.content) {
+      requestBody.messages.push(
+        {
+          role: 'system',
+          content: message.content.system_instructions,
+        },
+      )
+    }
+
+    if ('command_instructions' in message.content) {
+      requestBody.messages.push(
+        {
+          role: 'system',
+          content: message.content.command_instructions,
+        },
+      )
+    }
+
+    if ('additional_system_instructions' in body) {
+      requestBody.messages.push(
+        {
+          role: 'system',
+          content: body.additional_system_instructions,
+        },
+      )
+    }
+
+    if ('text' in message.content) {
+      requestBody.messages.push(
+        {
+          role: message.author,
+          content: message.content.text,
+        },
+      )
+    }
+
+    if ('temperature' in message.content)
+      temperature = message.content.temperature
+  }
+
+  return {
+    body: requestBody,
+    temperature,
+  }
+}
+
+/**
+ * Handle the AI completions into a stream response
+ * @param reply Fastify reply
+ * @param streamRequest the request to the AI
+ */
+export async function UniversalAICompletions(reply: FastifyReply, streamRequest: any) {
+  const completions = processStream(streamRequest).stream
+  return reply.sse((async function * source() {
+    try {
+      for await (const data of completions) {
+        const json = destr(data) as any
+        const res = {
+          text: json.choices[0]?.delta.content || '',
+        }
+        yield { data: JSON.stringify(res) }
+      }
+    }
+    catch (e: any) {
+      console.error('Error: ', e.message)
+      const res = {
+        text: '',
+        finish_reason: e.message,
+      }
+      yield { data: JSON.stringify(res) }
+    }
+    finally {
+      const res = {
+        text: '',
+        finish_reason: 'stop',
+      }
+      yield { data: JSON.stringify(res) }
+    }
+  })())
+}
