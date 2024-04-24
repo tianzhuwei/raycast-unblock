ee75cc8b92eca3cd5e998448580ab6e1afc3cfa4
Wibus <62133302+wibus-wee@users.noreply.github.com>
2024-04-24 22:12:23 +0800

    feat(internal): add authorize handler (#67)

--- src/routes/_internal/users.ts
+++ src/routes/_internal/users.ts
@@ -2,8 +2,25 @@ import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { Debug } from '../../utils/log.util'
 import { getStore } from '../../utils/store.util'
 import type { User } from '../../types'
+import { getConfig } from '../../utils/env.util'
 
 export function InternalUsersRoute(fastify: FastifyInstance, opts: Record<any, any>, done: Function) {
+  fastify.addHook('preHandler', async (request: FastifyRequest, reply: FastifyReply) => {
+    const { token } = request.headers
+    const debug = Debug.create('route:_internal:users')
+    const config = getConfig('general')?.token
+    if (config && token !== config) {
+      debug.warn('Unauthorized access')
+      return reply.code(401).send({ message: 'Unauthorized' })
+    }
+    else if (!config) {
+      debug.warn('Token not found in config')
+      return reply.code(401).send({ message: 'Token not found in config' })
+    }
+    else {
+      debug.info('Authorized access')
+    }
+  })
   fastify.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
     Debug.info('[GET] /users --> Internal API')
     const store = getStore<User[]>('users') || []
--- src/types/config/general.ts
+++ src/types/config/general.ts
@@ -12,4 +12,5 @@ export interface GeneralConfig {
     ca?: string
     host?: string
   }
+  token?: string
 }
